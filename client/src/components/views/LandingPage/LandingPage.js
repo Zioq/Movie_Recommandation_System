05de956764222d663b26b89e119eafc3a03ceaa3
Zioq <jr.roberthan@gmail.com>
2020-11-16 22:31:39 -0800

    update grid card

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,56 +1,65 @@
-import React,{useEffect,setState,useState} from 'react'
+import React, { useEffect, setState, useState } from "react";
 import { FaCode } from "react-icons/fa";
-import {API_URL,API_KEY,IMAGE_BASE_URL} from "../../Config";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
 import MainImage from "./MainImage";
+import Grid from "./Grid";
+import { Row } from "antd";
 
 function LandingPage() {
+  // Save the data
+  const [Movies, SetMovies] = useState([]);
+  // Save main Movie Image
+  const [MainMovieimg, setMainMovieimg] = useState(null);
 
-    // Save the data
-    const [Movies, SetMovies] =useState([]);
-    // Save main Movie Image 
-    const [MainMovieimg, setMainMovieimg] = useState(null);
+  //Get the Movie-Data from API
+  useEffect(() => {
+    const movieurl = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
+    //fecth the movie api
+    fetch(movieurl)
+      .then((response) => response.json())
+      .then((movieData) => {
+        console.log(movieData);
+        SetMovies(movieData.results);
+        setMainMovieimg(movieData.results[0]);
+      });
+  }, []);
 
+  return (
+    <div style={{ width: "100%", margin: "0" }}>
+      {/**To Avoid get the error result from rendering MainImage first*/}
+      {MainMovieimg && (
+        <MainImage
+          image={`${IMAGE_BASE_URL}/w1280${MainMovieimg.backdrop_path}`}
+          title={MainMovieimg.original_title}
+          description={MainMovieimg.overview}
+        />
+      )}
 
-    //Get the Movie-Data from API
-    useEffect(()=>{
-        const movieurl = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-
-        //fecth the movie api
-        fetch(movieurl)
-        .then(response=> response.json())
-        .then(movieData=>{
-            console.log(movieData);
-            SetMovies(movieData.results);
-            setMainMovieimg(movieData.results[0]);
-        });
-
-    },[])
-
-    return (
-      <div style={{width:'100%', margin:'0'}}>
-        
-        {/**To Avoid get the error result from rendering MainImage first*/}
-        {MainMovieimg && <MainImage 
-                            image={`${IMAGE_BASE_URL}/w1280${MainMovieimg.backdrop_path}`} 
-                            title={MainMovieimg.original_title}
-                            description={MainMovieimg.overview}
-                            />}
-        
-      <div style={{width: '85%', margin: '1rem auto'}}>
-        
+      <div style={{ width: "85%", margin: "1rem auto" }}>
         <h2>CHECK WHAT YOU WANT TO SEE :) </h2>
-        <hr/>
+        <hr />
+        
+        <Row gutter={[10,10]}>
+            {Movies && Movies.map((movie,index)=> (
+                <React.Fragment>
+                    <Grid 
+                        img={movie.poster_path? `${IMAGE_BASE_URL}/w400${movie.poster_path}`: null}
+                        movieId={movie.id}
+                        movieName={movie.original_title}
+                    />
+                </React.Fragment>
+            ))}
+        </Row>
 
 
       </div>
 
-        <div style={{display: 'flex', justifyContent:'center'}}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <button> SHOW MORE</button>
-        </div>
-
       </div>
-    )
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
